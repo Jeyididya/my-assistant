904327f1ffc191758c9e83f6528bea0530e8204a
Jeyididya <jedidiahandualem@gmail.com>
2025-12-24 00:20:26 +0300

    feat: fetch and reply to recent messages

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
 from decouple import config
+from telethon import TelegramClient, events
+from datetime import datetime, timedelta, timezone
 
 api_id = config("API_ID")
 api_hash = config("API_HASH")
 
-
-from telethon import TelegramClient, events
-
-
-
 client = TelegramClient("userbot_session", api_id, api_hash)
 
+REPLY_WINDOW_MINUTES = 5
+NOT_TO_MESSAGE = [777000]
 
-@client.on(events.NewMessage(incoming=True))
-async def handler(event):
-    if event.is_private:
-        await event.reply("hello")
+async def reply_to_recent_messages():
+    cutoff = datetime.now(timezone.utc) - timedelta(minutes=REPLY_WINDOW_MINUTES)
+
+    async for dialog in client.iter_dialogs():
+        if not dialog.is_user or dialog.entity.id in NOT_TO_MESSAGE:
+            continue
+        print(dialog.message.date, cutoff, dialog.name)
+        if dialog.message.date < cutoff:
+                break
+        async for msg in client.iter_messages(dialog.id):
+            print("->", msg.date, cutoff, dialog.name)
+            if msg.date < cutoff:
+                break
+                        
+            if msg.out:
+                continue
+
+            async for newer in client.iter_messages(dialog.id, min_id=msg.id):
+                if newer.out:
+                    break
+            else:
+                await msg.reply("hello")
+
+       
 
 
-client.start()
-client.run_until_disconnected()
+
+
+async def main():
+    await client.start()
+    print("Fetching recent messages")
+    await reply_to_recent_messages()
+    print("Done")
+
+
+with client:
+    client.loop.run_until_complete(main())
+
